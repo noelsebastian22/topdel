ec562fbc8156ef57ddc66497663695b4c3bfc9f5
Noel Sebastian <noelsimc69@gmail.com>
2026-07-06 20:53:49 +1000

    Initial commit: Topdel Astro site

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // TODO: set to the real domain before go-live, e.g. https://topdelhousedecor.com.au
+  compressHTML: true,
+});
