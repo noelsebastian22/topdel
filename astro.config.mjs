9101526d1a021fe1a857cc920aca54db2e51a03b
Noel Sebastian <noelsimc69@gmail.com>
2026-07-22 08:22:52 +1000

    Set site to https://topdelrenovation.com.au for go-live

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // TODO: set to the real domain before go-live, e.g. https://topdelrenovation.com.au
+  site: 'https://topdelrenovation.com.au',
   compressHTML: true,
 });
